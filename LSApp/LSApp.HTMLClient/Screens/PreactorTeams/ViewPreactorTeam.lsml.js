e1c1b9d46b1a968d53539140cfc68637dc4ba43e
Keerthi Jagalur Mutt <keerthijm@gmail.com>
2017-05-21 22:29:31 +0100

    LightSwitch Sample Application

---
+++ LSApp/LSApp.HTMLClient/Screens/PreactorTeams/ViewPreactorTeam.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewPreactorTeam.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.PreactorTeam.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.PreactorTeam." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
